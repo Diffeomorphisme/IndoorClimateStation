d5509f8b86a66123fb77dc9484d33b28d1339afd
Diffeomorphisme <nicolas.mxt@gmail.com>
2022-09-23 11:48:49 +0200

    Included env variables

--- src/main.py
+++ src/main.py
@@ -52,4 +52,4 @@ if __name__ == "__main__":
 	# Use this for debugging purposes only
 	import uvicorn
 
-	uvicorn.run(app, host="0.0.0.0", port=5000, log_level="debug")
+	uvicorn.run(app, host="0.0.0.0", port=8000, log_level="debug")
